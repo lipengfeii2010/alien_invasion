d109a9c5585906430e0247a353565ee47e90c15d
lipengfeii2010 <532632764@qq.com>
2019-02-21 17:19:02 +0800

    02.21modify

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
@@ -12,11 +13,12 @@ def run_game():
     pygame.display.set_caption("Aline Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
